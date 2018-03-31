e1ef648081d9cf0be8313566abe989b03a337557
hackopedia31 <wladi.schneider13@googlemail.com>
2018-03-31 22:38:36 +0200

    add models

---
+++ dezentrale_web/apps/general/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+
+class GiroPage(Page):
+    content = models.TextField()
+    content_panels = Page.content_panels + [
+        FieldPanel('content')
+    ]
+
+
+class InfrastrukturPage(Page):
+    content = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content')]
+
+
+class DokumentePage(Page):
+    content = StreamField([
+        ('Mitgliedsantrag', blocks.RichTextBlock()),
+        ('Misc', blocks.RichTextBlock()),
+        ('Protokoll', blocks.RichTextBlock())
+    ])
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content')]
+
+
+class ImpressumPage(Page):
+    content = StreamField([
+        ('Used', blocks.RichTextBlock())
+    ])
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content')]
